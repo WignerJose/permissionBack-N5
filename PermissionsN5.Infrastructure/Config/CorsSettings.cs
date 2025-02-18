0a14068d5c5c86fecc90d0060591f8f9e6108818
WignerJose <C.JWIGNE@tessa.com.uy>
2025-02-18 09:04:11 -0400

    update settings CORS

---
+++ PermissionsN5.Infrastructure/Config/CorsSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermissionsN5.Infrastructure.Config
+{
+    public class CorsSettings
+    {
+        public string[] AllowedOrigins { get; set; }
+    }
+}
--- PermissionsN5.WebApi/Program.cs
+++ PermissionsN5.WebApi/Program.cs
@@ -1,10 +1,23 @@
 using PermissionsN5.Application.DependencyInjection;
 using PermissionsN5.Application.Exceptions;
+using PermissionsN5.Infrastructure.Config;
 using PermissionsN5.Infrastructure.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<CorsSettings>(builder.Configuration.GetSection("CorsSettings"));
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            var allowedOrigins = builder.Configuration.GetSection("CorsSettings:AllowedOrigins").Get<string[]>();
+            policy.WithOrigins(allowedOrigins!)
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -25,6 +38,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowReactApp");
 
 app.UseAuthorization();
 
